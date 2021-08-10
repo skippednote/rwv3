4ff24d6e47f463603c6daa05b4ae24c9b99e12be
Bassam Ismail <skippednote@gmail.com>
2021-08-10 15:24:38 +0530

    03

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
+  <h1>Events for Good</h1>
   <router-view />
 </template>
 
---
+++ src/components/EventCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="event-card">
+    <span>@ {{ event.time }} {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+  props: {
+    event: Object,
+  },
+}
+</script>
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+</style>
